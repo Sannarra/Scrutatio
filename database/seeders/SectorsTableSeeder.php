6477b8f6bdedfd9eda6c5226f592c80353b56573
Andreas Leroux <andreas.leroux@epitech.eu>
2022-09-28 15:04:32 +0200

    Add Users seeder

--- database/seeders/SectorsTableSeeder.php
+++ database/seeders/SectorsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Sector;
+use Illuminate\Support\Facades\Schema;
 
 class SectorsTableSeeder extends Seeder
 {
@@ -17,8 +18,10 @@ class SectorsTableSeeder extends Seeder
     {
         $faker = \Faker\Factory::create();
 
+        Schema::disableForeignKeyConstraints();
         Sector::truncate();
-        // Create 50 product records
+        Schema::enableForeignKeyConstraints();
+
         for ($i = 0; $i < 50; $i++) {
             Sector::create([
                 'name' => $faker->name,
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        for ($i = 0; $i < 50; $i++) {
+            User::create([
+                'firstname' => $faker->firstName(),
+                'lastname' => $faker->lastName(),
+                'phone' => $faker->e164PhoneNumber(),
+                'email' => $faker->email(),
+                'city' => $faker->city(),
+                'status' => $faker->numberBetween(0, 5),
+                'username' => $faker->userName(),
+                'password' => hash('sha256', $faker->password())
+            ]);
+        }
+    }
+}
