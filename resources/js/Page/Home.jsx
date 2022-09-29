9abce36e144b9b050c754e5ce5b32f753afbacf4
Andreas Leroux <andreas.leroux@epitech.eu>
2022-09-29 22:40:07 +0200

    Set JobCard dynamique using react props

--- resources/js/Page/Home.jsx
+++ resources/js/Page/Home.jsx
@@ -1,7 +1,21 @@
-import FavoriteIcon from '@mui/icons-material/Favorite';
-
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import JobCard from "../components/JobCard";
 
 export default function Home() {
     const heading = "Scrutatio with Laravel and React";
-    return <div><FavoriteIcon /> {heading}</div>;
+    return (
+        <div>
+            <FavoriteIcon /> {heading}
+            <JobCard
+                jobTitle="Shampooer"
+                companyName="Wolf PLC"
+                city="Boyletown"
+                publication_date="29 sept 2022"
+                sectors={["Architecture", "Metal Production"]}
+                contract_type="CDI"
+                salary="1542"
+                working_time="35"
+            />
+        </div>
+    );
 }
--- resources/js/components/JobCard.jsx
+++ resources/js/components/JobCard.jsx
@@ -1,143 +1,170 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import StarIcon from '@mui/icons-material/StarBorder';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import Chip from '@mui/material/Chip';
-import Grid from '@mui/material/Unstable_Grid2';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import StarIcon from "@mui/icons-material/StarBorder";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Chip from "@mui/material/Chip";
+import Grid from "@mui/material/Unstable_Grid2";
 
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <Button {...other} />;
-})(({ theme, expand }) => ({
-  marginLeft: 'auto',
-  width: "130px",
-  borderRadius: 50
+const LearnMore = styled((props) => {
+    const { ...other } = props;
+    return <Button {...other} />;
+})(({ theme }) => ({
+    marginLeft: "auto",
+    width: "130px",
+    borderRadius: "50px",
 }));
 
-export default function JobCard() {
-  const [expanded, setExpanded] = React.useState(false);
-  const [buttonText, setButtonText] = React.useState('Learn more');
+class Sector extends React.Component {
+    render() {
+        return (
+            <Chip
+                label={this.props.name}
+                size="small"
+                sx={{
+                    backgroundColor: "#e1e5f2",
+                    marginLeft: "5px",
+                    marginRight: "5px",
+                }}
+            />
+        );
+    }
+}
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-    setButtonText(expanded ? "Learn more" : "hide")
-  };
+export default function JobCard(props) {
+    const [expanded, setExpanded] = React.useState(false);
+    const [buttonText, setButtonText] = React.useState("Learn more");
 
-  return (
-    <Card sx={{ maxWidth: 345, border:1, borderRadius: 3, boxShadow: 5}}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="company_icon" src="https://cdn.discordapp.com/icons/768816677491965973/89de8e72c08e16e921810da2a0fc5f19.webp">            
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="save" sx={{color: "#faaa00"}}>
-            <StarIcon />
-          </IconButton>
-        }
-        title="Shampooer"
-        subheader="Wolf PLC &#9679; Boyletown"
-        style={{ textAlign: 'center' }}
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.primary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <Typography variant="body2" color="text.secondary" sx={{marginLeft: 1}}>
-          29 sept 2022
-        </Typography>
-        <ExpandMore
-          variant="contained"
-          style={{backgroundColor: "#faaa00", color: "black"}}
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-        >
-          {buttonText}
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Divider sx={{marginBottom: "10px"}}>
-            <Chip label="Details" sx={{backgroundColor: "#e1e5f2"}}/>
-          </Divider>
-          <br/>
-          <Typography>Sectors: 
-            <Chip label="Agriculture" size="small" sx={{backgroundColor: "#e1e5f2", marginLeft: "5px", marginRight: "5px"}}/>
-            <Chip label="Metal Production" size="small" sx={{backgroundColor: "#e1e5f2", marginLeft: "5px", marginRight: "5px"}}/>
-          </Typography>
-          <br/>
-          <Typography>Contract Type: CDI</Typography>
-          <br/>
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+        setButtonText(expanded ? "Learn more" : "hide");
+    };
 
-          <Typography paragraph>Description:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-
-          <br/>
-          <br/>
-
-          <Grid container spacing={2}>
-            <Grid xs={1}/>
-            <Grid xs={5}>
-              <Typography variant="h6">
-                Salary
+    return (
+        <Card sx={{ maxWidth: 345, border: 1, borderRadius: 3, boxShadow: 5 }}>
+            <CardHeader
+                avatar={
+                    <Avatar
+                        aria-label="company_icon"
+                        src="https://cdn.discordapp.com/icons/768816677491965973/89de8e72c08e16e921810da2a0fc5f19.webp"
+                    />
+                }
+                action={
+                    <IconButton aria-label="save" sx={{ color: "#faaa00" }}>
+                        <StarIcon />
+                    </IconButton>
+                }
+                title={props.jobTitle}
+                subheader={`${props.companyName} ● ${props.city}`}
+                style={{ textAlign: "center" }}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.primary">
+                    This impressive paella is a perfect party dish and a fun
+                    meal to cook together with your guests. Add 1 cup of frozen
+                    peas along with the mussels, if you like.
                 </Typography>
-            </Grid>
-            <Grid xs={6}>
-            <Typography variant="h6">
-                Working Time
+            </CardContent>
+            <CardActions disableSpacing>
+                <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{ marginLeft: 1 }}
+                >
+                    {props.publication_date}
                 </Typography>
-            </Grid>
-            <Grid xs={1}/>
-            <Grid xs={5}>
-                1520$
-            </Grid>
-            <Grid xs={6}>
-                35h
-            </Grid>
-          </Grid>
+                <LearnMore
+                    variant="contained"
+                    style={{ backgroundColor: "#faaa00", color: "black" }}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                >
+                    {buttonText}
+                </LearnMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Divider sx={{ marginBottom: "10px" }}>
+                        <Chip
+                            label="Details"
+                            sx={{ backgroundColor: "#e1e5f2" }}
+                        />
+                    </Divider>
+                    <br />
+                    <Typography>
+                        Sectors:
+                        {props.sectors.map((sector, index) => {
+                            return <Sector name={sector} />;
+                        })}
+                    </Typography>
+                    <br />
+                    <Typography>
+                        Contract Type: {props.contract_type}
+                    </Typography>
+                    <br />
 
-          <br/>
-          <br/>
+                    <Typography paragraph>Description:</Typography>
+                    <Typography paragraph>
+                        Heat 1/2 cup of the broth in a pot until simmering, add
+                        saffron and set aside for 10 minutes.
+                    </Typography>
+                    <Typography paragraph>
+                        Heat oil in a (14- to 16-inch) paella pan or a large,
+                        deep skillet over medium-high heat. Add chicken, shrimp
+                        and chorizo, and cook, stirring occasionally until
+                        lightly browned, 6 to 8 minutes. Transfer shrimp to a
+                        large plate and set aside, leaving chicken and chorizo
+                        in the pan. Add pimentón, bay leaves, garlic, tomatoes,
+                        onion, salt and pepper, and cook, stirring often until
+                        thickened and fragrant, about 10 minutes. Add saffron
+                        broth and remaining 4 1/2 cups chicken broth; bring to a
+                        boil.
+                    </Typography>
+                    <Typography>
+                        Set aside off of the heat to let rest for 10 minutes,
+                        and then serve.
+                    </Typography>
 
-          <Button variant="contained" sx={{width:"100%", backgroundColor: "#faaa00", color: "black", borderRadius: 50}}>Apply</Button>
-        </CardContent>
-      </Collapse>
-    </Card>
-  );
+                    <br />
+                    <br />
+
+                    <Grid container spacing={2}>
+                        <Grid xs={1} />
+                        <Grid xs={5}>
+                            <Typography variant="h6">Salary</Typography>
+                        </Grid>
+                        <Grid xs={6}>
+                            <Typography variant="h6">Working Time</Typography>
+                        </Grid>
+                        <Grid xs={1} />
+                        <Grid xs={5}>{props.salary}$</Grid>
+                        <Grid xs={6}>{props.working_time}h</Grid>
+                    </Grid>
+
+                    <br />
+                    <br />
+
+                    <Button
+                        variant="contained"
+                        sx={{
+                            width: "100%",
+                            backgroundColor: "#faaa00",
+                            color: "black",
+                            borderRadius: 50,
+                        }}
+                    >
+                        Apply
+                    </Button>
+                </CardContent>
+            </Collapse>
+        </Card>
+    );
 }
