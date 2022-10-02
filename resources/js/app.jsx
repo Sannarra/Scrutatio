d050a72477012c03db77a3511d13a4a48538c70c
Sannarra <lisa.bourdon@epitech.eu>
2022-10-02 23:27:34 +0200

    Remove the Sidebar import

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,11 +4,9 @@ import '../css/app.css'
 import ReactDOM from 'react-dom/client';        
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
 
 ReactDOM.createRoot(document.getElementById('app')).render(     
     <>
-        <Sidebar />
         <Header />
         <Footer />
     </>
