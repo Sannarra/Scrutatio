eee5de21249f98fb47204d1dd9df94ba0c3014c1
Sannarra <lisa.bourdon@epitech.eu>
2022-10-04 15:54:01 +0200

    Join Header/Footer and Sidebar

--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -2,15 +2,22 @@ import * as React from "react";
 import Sidebar from "./components/Sidebar.jsx";
 import Button from "@mui/material/Button";
 import Home from "./Page/Home.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import { set } from "lodash";
 
 export default function Main(props) {
     const [sidebarOpen, setSidebarOpen] = React.useState(false);
 
     return (
-        <>
-            <Button onClick={() => setSidebarOpen(true)}>Hello</Button>
-            <Home data={props.data} />
-            <Sidebar isOpen={sidebarOpen} setOpen={setSidebarOpen} />
-        </>
+        <div style={{ display: "flex", flexDirection: "column" , flex: '1', height:'100vh'}}>
+            <Header openSidebar={() => setSidebarOpen(true)}    
+            />
+            <div style={{ overflowY: 'auto', flex:'1', paddingTop:'10px' }}>
+                <Home data={props.data} />
+                <Sidebar isOpen={sidebarOpen} setOpen={setSidebarOpen} />
+                <Footer />
+            </div>
+        </div>
     );
 }
--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -4,11 +4,13 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 
-export default function Header() {
+export default function Header({ openSidebar }) {
     return (
-        <header>
-            <div><SearchIcon/></div>
-            <div>Scrutatio</div>
+        <header style={{maxHeight:'5vh'}}>
+            <button onClick={() => openSidebar()}><SearchIcon/></button>
+            <div style={{justifyContent:'center',alignItems:'center', display:'flex'}} >
+                <img style={{width:'100%',height:'auto', maxHeight:"100%" }} src="./logo-white.png" alt="logo" />
+            </div>
             <div>
                 <div><a href='#'><ChatIcon/></a></div>
                 <div><a href='#'><AccountCircleIcon/></a></div>  
