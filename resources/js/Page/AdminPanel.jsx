f1dd07a2d01f8d665c059c3195fb26cfdf045c97
Andreas Leroux <andreas.leroux@epitech.eu>
2022-10-07 17:49:56 +0200

    Add Companies and Users Grid in Admin Panel

--- resources/js/Page/AdminPanel.jsx
+++ resources/js/Page/AdminPanel.jsx
@@ -1,11 +1,15 @@
 import PostsGrid from "../components/grid/PostsGrid.jsx";
 import AccountsGrid from "../components/grid/AccountsGrid.jsx";
+import CompaniesGrid from "../components/grid/CompaniesGrid.jsx";
+import UsersGrid from "../components/grid/UsersGrid.jsx";
 
 export default function AdminPanel(props) {
     return (
         <div>
             <PostsGrid />
             <AccountsGrid />
+            <CompaniesGrid />
+            <UsersGrid />
         </div>
     );
 }
