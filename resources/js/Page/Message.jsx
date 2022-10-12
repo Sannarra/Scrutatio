36e8a94a8180248ba54aa048ed9161e73c4551e1
Sannarra <lisa.bourdon@epitech.eu>
2022-10-12 15:15:00 +0200

    add margin footer chat

--- resources/js/Page/Message.jsx
+++ resources/js/Page/Message.jsx
@@ -72,7 +72,7 @@ export default function Message(props) {
     };
 
     return (
-        <Container>
+        <Container sx={{mb:'20px'}}>
             {
                 // Display a list of all conversations with a button to select each conversation
                 conversations.map((conv) => {
@@ -88,181 +88,194 @@ export default function Message(props) {
                     );
                 })
             }
-{conversations.length == 0 ? (
-            <div
-                style={{
-                    backgroundColor: "var(--dark)",
-                    padding: "10px",
-                    borderRadius: "5px",
-                }}
-            >
-                {/* if not apply -> find offer else -> click offer */}
-                {conversations.length == 0 ? (
-                    <Container
-                        sx={{
-                            bgcolor: "white",
-                            fontSize: "2em",
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            padding: "5%",
-                            textAlign: "center",
-                        }}
-                    >
-                        <div>You have not applied to any job offers yet!</div>
-                        <Button
-                            variant="contained"
-                            href="/"
-                            sx={{ margin: "3%" }}
+           {currentConversation === null ? (
+                                <></>
+                            ) : (
+                <div
+                    style={{
+                        backgroundColor: "var(--dark)",
+                        padding: "10px",
+                        borderRadius: "5px",
+                    }}
+                >
+                    {/* if not apply -> find offer else -> click offer */}
+                    {conversations.length == 0 ? (
+                        <Container
+                            sx={{
+                                bgcolor: "white",
+                                fontSize: "2em",
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                padding: "5%",
+                                textAlign: "center",
+                            }}
                         >
-                            Find offers?
-                        </Button>
-                    </Container>
-                ) : (
-                    <></>
-                )}
-
-                {/* if not apply -> find offer else -> click offer */}
-                <Grid>
-                    <div>
-                        {currentConversation != null ? (
-                            <div
-                                style={{
-                                    backgroundColor: "var(--dark)",
-                                    color: "var(--light)",
-                                    display: "flex",
-                                    textAlign: "center",
-                                    alignItems: "center",
-                                    gap: "3%",
-                                    padding: "3vh",
-                                }}
+                            <div>
+                                You have not applied to any job offers yet!
+                            </div>
+                            <Button
+                                variant="contained"
+                                href="/"
+                                sx={{ margin: "3%" }}
                             >
-                                {/* Todo: adapt avatar */}
+                                Find offers?
+                            </Button>
+                        </Container>
+                    ) : (
+                        <></>
+                    )}
 
-                                <Avatar sx={{ bgcolor: "orange" }}>
+                    {/* if not apply -> find offer else -> click offer */}
+                    <Grid>
+                        <div>
+                            {currentConversation != null ? (
+                                <div
+                                    style={{
+                                        backgroundColor: "var(--dark)",
+                                        color: "var(--light)",
+                                        display: "flex",
+                                        textAlign: "center",
+                                        alignItems: "center",
+                                        gap: "3%",
+                                        padding: "3vh",
+                                    }}
+                                >
+                                    {/* Todo: adapt avatar */}
+
+                                    <Avatar sx={{ bgcolor: "orange" }}>
+                                        {
+                                            conversations.filter(
+                                                (conv) =>
+                                                    conv.id ==
+                                                    currentConversation
+                                            )[0]?.title[0]
+                                        }
+                                    </Avatar>
                                     {
                                         conversations.filter(
                                             (conv) =>
                                                 conv.id == currentConversation
-                                        )[0]?.title[0]
+                                        )[0]?.title
                                     }
-                                </Avatar>
-                                {
-                                    conversations.filter(
-                                        (conv) => conv.id == currentConversation
-                                    )[0]?.title
-                                }
-                            </div>
-                        ) : null}
-                    </div>
-                </Grid>
-                {/* content */}
+                                </div>
+                            ) : null}
+                        </div>
+                    </Grid>
+                    {/* content */}
 
-                <Grid>
-                    <div>
-                        {currentConversation === null ? (
-                            <></>
-                        ) : messages.length === 0 ? (
-                            <div
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    height: "20vh",
-                                    backgroundColor:"white"
-                                }}
-                            >
-                                <h1>Send a message to get started!</h1>
-                            </div>
-                        ) : (
-                            messages.map((message, i) => {
-                                return (
-                                    <div
-                                        key={message.id}
-                                        style={{ width: "100%" }}
-                                    >
-                                        <Grid
-                                            style={{
-                                                backgroundColor:
-                                                    i % 2 == 0
-                                                        ? "var(--accent2)"
-                                                        : "var(--background)",
-                                                marginTop: "20px",
-                                                marginLeft:
-                                                    i % 2 == 0 ? "10%" : 0,
-                                                marginRight:
-                                                    i % 2 == 0 ? 0 : "10%",
-                                                borderRadius: "6px",
-
-                                                padding: "5px",
-                                            }}
+                    <Grid>
+                        <div>
+                            {currentConversation === null ? (
+                                <></>
+                            ) : messages.length === 0 ? (
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        height: "20vh",
+                                        backgroundColor: "white",
+                                    }}
+                                >
+                                    <h1>Send a message to get started!</h1>
+                                </div>
+                            ) : (
+                                messages.map((message, i) => {
+                                    return (
+                                        <div
+                                            key={message.id}
+                                            style={{ width: "100%" }}
                                         >
-                                            <span
-                                                style={{ fontWeight: "bold" }}
-                                            >
-                                                {message.sender_name}
-                                            </span>
-                                            <br /> 
-                                            <div style={{whiteSpace:'pre-wrap'}}>
-                                            {message.content}
-                                            </div>
-                                            <br />
-                                            <Typography
-                                                color="text.secondary"
-                                                style={{ fontSize: ".7em" }}
-                                            >
-                                                {new Date(
-                                                    message.created_at
-                                                ).toLocaleString("fr-FR")}
-                                            </Typography>
-                                        </Grid>
-                                    </div>
-                                );
-                            })
-                        )}
-                    </div>
-                </Grid>
+                                            <Grid
+                                                style={{
+                                                    backgroundColor:
+                                                        i % 2 == 0
+                                                            ? "var(--accent2)"
+                                                            : "var(--background)",
+                                                    marginTop: "20px",
+                                                    marginLeft:
+                                                        i % 2 == 0 ? "10%" : 0,
+                                                    marginRight:
+                                                        i % 2 == 0 ? 0 : "10%",
+                                                    borderRadius: "6px",
 
-                {/* sub */}
-                {currentConversation === null ? (
-                    <></>
-                ) : (
-                    <div
-                        style={{
-                            display: "flex",
-                            alignItems: "end",
-                            justifyContent:'center',
-                            width: "100%",
-                            padding: 0,
-                        }}
-                    >
-                        <TextField
-                            multiline
-                            placeholder="Send a message"
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
+                                                    padding: "5px",
+                                                }}
+                                            >
+                                                <span
+                                                    style={{
+                                                        fontWeight: "bold",
+                                                    }}
+                                                >
+                                                    {message.sender_name}
+                                                </span>
+                                                <br />
+                                                <div
+                                                    style={{
+                                                        whiteSpace: "pre-wrap",
+                                                    }}
+                                                >
+                                                    {message.content}
+                                                </div>
+                                                <br />
+                                                <Typography
+                                                    color="text.secondary"
+                                                    style={{ fontSize: ".7em" }}
+                                                >
+                                                    {new Date(
+                                                        message.created_at
+                                                    ).toLocaleString("fr-FR")}
+                                                </Typography>
+                                            </Grid>
+                                        </div>
+                                    );
+                                })
+                            )}
+                        </div>
+                    </Grid>
+
+                    {/* sub */}
+                    {currentConversation === null ? (
+                        <></>
+                    ) : (
+                        <div
                             style={{
+                                display: "flex",
+                                alignItems: "end",
+                                justifyContent: "center",
                                 width: "100%",
-                                borderColor: "black",
-                                backgroundColor: "lightgrey",
-                                border: "0",
-                                textJustify: "center",
-                                marginTop: '20px'
+                                padding: 0,
                             }}
-                        />
-
-                        {/* input */}
-
-                        <IconButton
-                            sx={{ color: "orange", mt: '20px'}}
-                            onClick={() => send()}
                         >
-                            <SendIcon />
-                        </IconButton>
-                    </div>
-                )}
-            </div>
+                            <TextField
+                                multiline
+                                placeholder="Send a message"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                style={{
+                                    width: "100%",
+                                    borderColor: "black",
+                                    backgroundColor: "lightgrey",
+                                    border: "0",
+                                    textJustify: "center",
+                                    marginTop: "20px",
+                                }}
+                            />
+
+                            {/* input */}
+
+                            <IconButton
+                                sx={{ color: "orange"}}
+                                onClick={() => send()}
+                            >
+                                <SendIcon />
+                            </IconButton>
+                        </div>
+                    )}
+                </div>
+            ) }
         </Container>
     );
 }
